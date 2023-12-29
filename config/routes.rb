ee9fc3088c6f23ced0c7883c0e7b2e0bf69f4b87
Oscar Cunduri <oscar.cunduri@epn.edu.ec>
2023-12-29 12:25:41 -0500

    Some refactors, added home page file

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,5 @@
 Rails.application.routes.draw do
   resources :types, except: :show
-  resources :cars, path: '/'
-=begin
-  get '/cars/new' => 'cars#new', as: :new_car
-  get "cars" => "cars#index", as: :cars
-  get 'cars/:id' => 'cars#show', as: :car
-  get 'cars/:id/edit' => 'cars#edit', as: :edit_car
-  post '/cars' => 'cars#create'
-  patch '/cars/:id' => 'cars#update'
-  delete '/cars/:id' => 'cars#destroy'
-=end
-
-  #Se reemplaza todas las rutas anteriores con el comando siguiente
-
-
+  resources :cars
+  root 'home#index'
 end
